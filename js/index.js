bb2ad023ed8fdd8c4f740df5ef424bfce19e3c45
MikeL Kamel <mikel1832@yahoo.com>
2021-09-19 09:17:04 -0400

    Layout Changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
-
-  $('.second-button').on('click', function () {
-
-    $('.animated-icon').toggleClass('open');
+  $(".second-button").on("click", function () {
+    $(".animated-icon").toggleClass("open");
   });
 
+  // Parallax.js
+  var scene = $("#scene").get(0);
+  var parallaxInstance = new Parallax(scene);
 });
+
+document.querySelector(".title-img-container").style.opacity = "1";
+document.querySelector(".my-name-container").style.opacity = "1";
