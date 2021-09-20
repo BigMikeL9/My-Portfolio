89f72f0b4f57917b874c8bb4a1ca0dba524d9a4d
MikeL Kamel <mikel1832@yahoo.com>
2021-09-20 17:39:11 -0400

    add Kardio dev app

--- js/index.js
+++ js/index.js
@@ -2,10 +2,6 @@ $(document).ready(function () {
   $(".second-button").on("click", function () {
     $(".animated-icon").toggleClass("open");
   });
-
-  // Parallax.js
-  var scene = $("#scene").get(0);
-  var parallaxInstance = new Parallax(scene);
 });
 
 document.querySelector(".title-img-container").style.opacity = "1";
