7ee7e80e9677cfdab49ac42efef75e7a6955d2c7
MikeL Kamel <mikel1832@yahoo.com>
2021-09-24 19:20:29 -0400

    about me section update and project desc and header animations

--- js/index.js
+++ js/index.js
@@ -6,3 +6,4 @@ $(document).ready(function () {
 
 document.querySelector(".title-img-container").style.opacity = "1";
 document.querySelector(".my-name-container").style.opacity = "1";
+document.querySelector(".title__curtain").style.height = "0";
